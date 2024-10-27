49fb05288d4ab20554e6a280e22603439ef5f2d7
Goldfei <424398453@qq.com>
2024-10-27 20:10:33 +0800

    course_8

---
+++ contracts/2_ValueTypesDemo.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ValueTypesDemo {
+    int public defaultInt;
+    uint public defaultUint;
+    bool public defaultBool;
+    address public defaultAddress;
+    bytes1 public defaultBytes1;
+
+    enum ActionSet { Buy, Hold, Sell }
+    ActionSet public defaultEnum;
+}
