4531c8f21a1649126e335dc3dd2805bb493d0358
Goldfei <424398453@qq.com>
2024-11-29 00:07:20 +0800

    1127

--- contracts/course_demo/26_EventDemo.sol
+++ contracts/course_demo/26_EventDemo.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract EventDemo {
+    // 定义一个事件
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    // 模拟转账函数
+    function transfer(address to, uint256 amount) public {
+        // 触发事件
+        emit Transfer(msg.sender, to, amount);
+    }
+}
